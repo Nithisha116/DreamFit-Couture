ac13dfd658c12b4aab50fd7f576932e02ce97eb2
Shaaz Alfaiz <shaazalfaiz25@gmail.com>
2026-06-10 13:32:20 +0530

    Fix: Remove cache-control headers from Axios that broke CORS preflight

--- client/src/app/axios.js
+++ client/src/app/axios.js
@@ -11,9 +11,6 @@ const API = axios.create({
   baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
-    "Cache-Control": "no-cache",
-    "Pragma": "no-cache",
-    "Expires": "0",
   },
 });
 
